766ce23e08dcd5c0173661fc5be6085db35bec77
Joseph Reynolds <jreynolds@infocraft.net>
2016-01-28 14:32:44 -0600

    Initial commit

---
+++ src/SimpleInjection/Core/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Core
+{
+    public class Person
+    {
+        public Person()
+        {
+        }
+
+        public Guid PersonId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
